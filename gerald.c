630d8bafc3ed4141ab865f9e8a76e5f92a9c2fdc
Todd Agulnick <todd@agulnick.com>
2013-12-27 21:01:53 -0500

    More crap in progress.

--- gerald.c
+++ gerald.c
@@ -43,10 +43,12 @@ typedef struct {
   GMainLoop  *loop;
   GstElement *pipeline;
   GstElement *source;
+  GstElement *videorate;
   GstElement *converter;
   GstElement *queue1;
   GstElement *encoder;
   GstElement *queue2;
+  GstElement *bin;
   GstElement *mux;
   GstElement *sink;
   app_status status;
@@ -54,45 +56,8 @@ typedef struct {
   gulong block_probe_id;
   guint file_count;
   gboolean first;
+  gboolean blocking;
 } app_data;
-
-static gboolean
-bus_call (GstBus     *bus,
-    GstMessage *msg,
-    gpointer    data)
-{
-  GMainLoop *loop = (GMainLoop *) data;
-
-  switch (GST_MESSAGE_TYPE (msg)) {
-
-    case GST_MESSAGE_EOS:
-      g_print ("End of stream\n");
-      g_main_loop_quit (loop);
-      break;
-
-    case GST_MESSAGE_ERROR:
-      {
-        gchar  *debug;
-        GError *error;
-
-        gst_message_parse_error (msg, &error, &debug);
-
-        g_printerr ("Error: %s\n", error->message);
-        g_error_free (error);
-
-        g_printerr ("Debugging info: %s\n", (debug) ? debug : "none");
-        g_free (debug);
-
-        g_main_loop_quit (loop);
-        break;
-      }
-    default:
-      break;
-  }
-
-  return TRUE;
-}
-
 
 void set_state_and_wait (GstElement * element, GstState state, app_data * app)
 {
@@ -121,6 +86,55 @@ void set_state_and_wait (GstElement * element, GstState state, app_data * app)
   }
 }
 
+void drop_mux_and_sink (app_data * app)
+{
+  g_printf ("Unlinking and removing bin...\n");
+  gst_element_unlink (app->queue2, app->bin);
+  set_state_and_wait(app->bin, GST_STATE_NULL, app);
+  gst_bin_remove (GST_BIN(app->pipeline), app->bin);
+  g_printf("done!\n");
+}
+
+
+static gboolean
+bus_call (GstBus     *bus,
+    GstMessage *msg,
+    gpointer    data)
+{
+  app_data * app = data;
+
+  switch (GST_MESSAGE_TYPE (msg)) {
+
+    case GST_MESSAGE_EOS:
+      g_print ("End of stream detected on bus\n");
+      drop_mux_and_sink (app);
+      /*g_main_loop_quit (app->loop);*/
+      break;
+
+    case GST_MESSAGE_ERROR:
+      {
+        gchar  *debug;
+        GError *error;
+
+        gst_message_parse_error (msg, &error, &debug);
+
+        g_printerr ("Error: %s\n", error->message);
+        g_error_free (error);
+
+        g_printerr ("Debugging info: %s\n", (debug) ? debug : "none");
+        g_free (debug);
+
+        g_main_loop_quit (app->loop);
+        break;
+      }
+    default:
+      break;
+  }
+
+  return TRUE;
+}
+
+
 #if 0
 static void
 on_pad_added (GstElement *element,
@@ -141,79 +155,45 @@ on_pad_added (GstElement *element,
 }
 #endif
 
-void drop_mux_and_sink (app_data * app)
-{
-  gst_element_set_state (app->mux, GST_STATE_NULL);
-
-  g_printf("setting sink to NULL\n");
-
-  set_state_and_wait (app->sink, GST_STATE_NULL, app);
-
-  g_printf ("removing mux and sink...\n");
-  gst_bin_remove (GST_BIN(app->pipeline), app->mux);
-  gst_bin_remove (GST_BIN(app->pipeline), app->sink);
-  g_printf("done!\n");
-
-}
-
-
+#if 0
 static GstPadProbeReturn
 eos_probe_cb (GstPad * pad, GstPadProbeInfo * info, gpointer user_data)
 {
   app_data * app = user_data;
+  GstEvent * event;
+
+  g_printf ("In eos_probe_cb with %p, %p, and %p\n", pad, info, user_data);
+
+  event = gst_pad_probe_info_get_event (info);
+
+  g_printf ("event type from event is %x\n", event->type);
 
   if (GST_EVENT_TYPE (GST_PAD_PROBE_INFO_DATA (info)) != GST_EVENT_EOS) {
-    g_printf ("got another event that wasn't our EOS; ignoring\n");
+    g_printf ("got another event type %x that wasn't our EOS %x; ignoring\n", GST_EVENT_TYPE(GST_PAD_PROBE_INFO_DATA(info)), GST_EVENT_EOS);
     return GST_PAD_PROBE_OK;
   }
 
-  g_printf("Received EOS event\n");
+  g_printf("Received EOS event!\n");
 
   gst_pad_remove_probe (pad, GST_PAD_PROBE_INFO_ID (info));
 
-  drop_mux_and_sink(app);
+  /*drop_mux_and_sink(app);*/
 
-  return GST_PAD_PROBE_DROP;
+  return GST_PAD_PROBE_OK;
 }
+#endif
 
 
 static GstPadProbeReturn
 blockpad_probe_cb (GstPad * pad, GstPadProbeInfo * info, gpointer user_data)
 {
-  /*GstBuffer *buffer = gst_pad_probe_info_get_buffer(info);*/
-  /*GstClockTime pts = GST_BUFFER_PTS(buffer);*/
-  /*GstClockTime dts = GST_BUFFER_DTS(buffer);*/
-
-  /*g_print ("blockpad_probe_cb: pts = %ld dts = %ld\n", pts, dts);*/
-
-  gst_pad_remove_probe (pad, GST_PAD_PROBE_INFO_ID (info));
-
   app_data * app = user_data;
+
+  /*gst_pad_remove_probe (pad, GST_PAD_PROBE_INFO_ID (info));*/
 
   GST_DEBUG_OBJECT (pad, "pad is blocked now");
 
-  g_printf ("Unlinking mux...\n");
-  gst_element_unlink (app->queue2, app->mux);
-
-  g_printf ("Pausing queue...\n");
-  set_state_and_wait ( app->queue2, GST_STATE_PAUSED, app);
-
-  g_printf ("Setting event probe...\n");
-  GstPad * srcpad = gst_element_get_static_pad (app->mux, "src");
-  gst_pad_add_probe (srcpad, GST_PAD_PROBE_TYPE_BLOCK_DOWNSTREAM | GST_PAD_PROBE_TYPE_EVENT_DOWNSTREAM, eos_probe_cb, app, NULL);
-  gst_object_unref (srcpad);
-
-  g_printf ("Getting sinkpad... ");
-  GstPad * sinkpad = gst_element_get_static_pad (app->mux, "video_0");
-  g_printf ("sinkpad is %p\n", sinkpad);
-
-  g_printf ("Generating new EOS... ");
-  GstEvent * event = gst_event_new_eos ();
-  g_printf ("event is %p\n", event);
-
-  g_printf ("Sending event...\n");
-  gst_pad_send_event (sinkpad, event);
-  gst_object_unref (sinkpad);
+  gst_element_send_event (app->mux, gst_event_new_eos());
 
   g_printf("Sent! Now we wait.\n");
   return GST_PAD_PROBE_OK;
@@ -231,24 +211,34 @@ void block_pipeline(app_data *app)
 
 void unblock_pipeline(app_data *app, char * output_location)
 {
+  GstPad * sinkpad;
+
   g_printf ("Saving stream to %s...\n", output_location);
 
+  app->bin = gst_element_factory_make ("bin", "bin");
   app->mux = gst_element_factory_make ("mp4mux", "mux");
   app->sink = gst_element_factory_make ("filesink", "sink");
 
+  if (!app->bin) { g_printerr("Failed to create bin"); }
   if (!app->mux) { g_printerr("Failed to create mux"); }
   if (!app->sink) { g_printerr("Failed to create sink"); }
 
-  if (!app->mux || !app->sink) {
+  if (!app->bin || !app->mux || !app->sink) {
     g_main_loop_quit (app->loop);
   }
 
   g_object_set (app->sink, "location", output_location, NULL);
-  gst_bin_add_many (GST_BIN (app->pipeline), app->mux, app->sink, NULL);
-  gst_element_link_many (app->queue2, app->mux, app->sink, NULL);
 
-  gst_element_sync_state_with_parent (app->mux);
-  gst_element_sync_state_with_parent (app->sink);
+  gst_bin_add_many (GST_BIN (app->bin), app->mux, app->sink, NULL);
+  gst_element_link_many (app->mux, app->sink, NULL);
+
+  gst_element_add_pad(app->bin, gst_ghost_pad_new_no_target ("sink", GST_PAD_SINK));
+
+  gst_bin_add (GST_BIN(app->pipeline), app->bin);
+
+  gst_element_link (app->queue2, app->bin);
+
+  gst_element_sync_state_with_parent (app->bin);
 
   if (app->block_probe_id) {
     gst_pad_remove_probe (app->blockpad, app->block_probe_id);
@@ -272,9 +262,12 @@ gboolean io_callback(GIOChannel *source, GIOCondition condition, gpointer data)
         unblock_pipeline(app, file_name);
       } else if (!strcmp("stop", buffer->str)) {
         block_pipeline(app);
+      } else if (!strcmp("pause", buffer->str)) {
+        set_state_and_wait (app->pipeline, GST_STATE_PAUSED, app);
       } else if (!strcmp("shutdown", buffer->str)) {
         g_print("Shutting down\n");
-        g_main_loop_quit (app->loop);
+        gst_element_send_event (app->pipeline, gst_event_new_eos ());
+        /*g_main_loop_quit (app->loop);*/
       } else {
         g_print("Unrecognized command\n");
       }
@@ -295,6 +288,7 @@ gboolean io_callback(GIOChannel *source, GIOCondition condition, gpointer data)
   return TRUE;
 }
 
+#if 0
 static gboolean
 timeout_cb (gpointer user_data)
 {
@@ -305,6 +299,7 @@ timeout_cb (gpointer user_data)
 
   return FALSE;
 }
+#endif
 
 int
 main (int   argc,
@@ -312,18 +307,19 @@ main (int   argc,
 {
   GstBus *bus;
   guint bus_watch_id;
-  app_data app;
+  app_data ad;
+  app_data * app = &ad;
 
   /* Initialisation */
   gst_init (&argc, &argv);
 
-  app.status = STATUS_IDLE;
-  app.block_probe_id = 0;
-  app.file_count = 0;
-  app.sink = NULL;
-  app.mux = NULL;
-  app.loop = g_main_loop_new (NULL, FALSE);
-  app.first = TRUE;
+  app->status = STATUS_IDLE;
+  app->block_probe_id = 0;
+  app->file_count = 0;
+  app->sink = NULL;
+  app->mux = NULL;
+  app->loop = g_main_loop_new (NULL, FALSE);
+  app->first = TRUE;
 
   /* Check input arguments */
   /*if (argc != 2) {*/
@@ -333,21 +329,29 @@ main (int   argc,
 
 
   /* Create gstreamer elements */
-  app.pipeline  = gst_pipeline_new ("gerald");
-  app.source    = gst_element_factory_make ("videotestsrc", "video-source");
-  app.converter = gst_element_factory_make ("videoconvert", "video-convert");
-  app.queue1    = gst_element_factory_make ("queue", "upstream-queue");
-  app.encoder   = gst_element_factory_make ("x264enc", "video-encoder");
-  app.queue2    = gst_element_factory_make ("queue", "downstream-queue");
+  app->pipeline  = gst_pipeline_new ("gerald");
+  app->source    = gst_element_factory_make ("videotestsrc", "video-source");
+  app->videorate = gst_element_factory_make ("videorate", "video-rate");
+  app->converter = gst_element_factory_make ("videoconvert", "video-convert");
+  app->queue1    = gst_element_factory_make ("queue", "upstream-queue");
+  app->encoder   = gst_element_factory_make ("x264enc", "video-encoder");
+  app->queue2    = gst_element_factory_make ("queue", "ringbuffer-queue");
 
-  if (!app.pipeline) { g_printerr ("Failed to create pipeline"); }
-  if (!app.source) { g_printerr("failed to create videotestsrc"); }
-  if (!app.converter) { g_printerr("failed to create videoconvert"); }
-  if (!app.queue1) { g_printerr("failed to create upstream-queue"); }
-  if (!app.encoder) { g_printerr("failed to create encoder"); }
-  if (!app.queue2) { g_printerr("failed to create downstream-queue"); }
+  if (!app->pipeline) { g_printerr ("Failed to create pipeline"); }
+  if (!app->source) { g_printerr("failed to create videotestsrc"); }
+  if (!app->videorate) { g_printerr("failed to create video-rate"); }
+  if (!app->converter) { g_printerr("failed to create videoconvert"); }
+  if (!app->queue1) { g_printerr("failed to create upstream-queue"); }
+  if (!app->encoder) { g_printerr("failed to create encoder"); }
+  if (!app->queue2) { g_printerr("failed to create ringbuffer-queue"); }
 
-  if (!app.pipeline || !app.source || !app.converter || !app.queue1 || !app.encoder || !app.queue2) {
+  if (!app->pipeline ||
+      !app->source ||
+      !app->videorate ||
+      !app->converter ||
+      !app->queue1 ||
+      !app->encoder ||
+      !app->queue2) {
     g_printerr ("An element could not be created. Exiting.\n");
     return -1;
   }
@@ -355,40 +359,38 @@ main (int   argc,
   g_print ("Elements added, setting props\n");
 
 #define GST_QUEUE_LEAK_DOWNSTREAM 2
-  g_object_set (app.queue2, "max-size-bytes", 1024 * 1024 * 1024, NULL);
-  g_object_set (app.queue2, "leaky", GST_QUEUE_LEAK_DOWNSTREAM, NULL);
+  g_object_set (app->source, "is-live", TRUE, NULL);
+  g_object_set (app->queue2, "max-size-bytes", 10 * 1024 * 1024, NULL);
+  g_object_set (app->queue2, "leaky", GST_QUEUE_LEAK_DOWNSTREAM, NULL);
 
   /* Set up the pipeline */
 
-  /* we set the input filename to the source element */
-  /*g_object_set (G_OBJECT (source), "location", argv[1], NULL);*/
-
-  /* we add a message handler */
-  bus = gst_pipeline_get_bus (GST_PIPELINE (app.pipeline));
-  bus_watch_id = gst_bus_add_watch (bus, bus_call, app.loop);
+  /* Add a message handler */
+  bus = gst_pipeline_get_bus (GST_PIPELINE (app->pipeline));
+  bus_watch_id = gst_bus_add_watch (bus, bus_call, app);
   gst_object_unref (bus);
 
   g_print("Adding elements...\n");
-  /* we add all elements into the pipeline */
-  gst_bin_add_many (GST_BIN (app.pipeline),
-      app.source, app.converter, app.queue1, app.encoder, app.queue2, NULL);
+  /* Add elements up to the second queue -- the "fixed" part of the pipeline */
+  gst_bin_add_many (GST_BIN (app->pipeline),
+      app->source, app->videorate, app->converter, app->queue1, app->encoder, app->queue2, NULL);
 
   g_print("Linking elements...\n");
   /* we link the elements together */
   gst_element_link_many (
-      app.source, app.converter, app.queue1, app.encoder, app.queue2, NULL);
+      app->source, app->videorate, app->converter, app->queue1, app->encoder, app->queue2, NULL);
 
-  app.blockpad = gst_element_get_static_pad (app.queue2, "src");
+  app->blockpad = gst_element_get_static_pad (app->queue2, "src");
 
-  unblock_pipeline (&app, "/dev/null");
+  unblock_pipeline (app, "/dev/null");
 
   // Verbose
-  // g_signal_connect (app.pipeline, "deep-notify",
-  //  G_CALLBACK (gst_object_default_deep_notify), NULL);
+   g_signal_connect (app->pipeline, "deep-notify",
+    G_CALLBACK (gst_object_default_deep_notify), NULL);
 
   /* Set the pipeline to "playing" state*/
   g_print ("Starting pipeline...\n");
-  set_state_and_wait (app.pipeline, GST_STATE_PLAYING, &app);
+  set_state_and_wait (app->pipeline, GST_STATE_PLAYING, app);
 
   /*block_pipeline (&app);*/
 
@@ -400,30 +402,22 @@ main (int   argc,
   mkfifo(control_pipe, 0666);
   int pipe = g_open(control_pipe, O_RDONLY, O_NONBLOCK);
   io = g_io_channel_unix_new (pipe);
-  io_watch_id = g_io_add_watch (io, G_IO_IN, io_callback, &app);
+  io_watch_id = g_io_add_watch (io, G_IO_IN, io_callback, app);
   g_io_channel_unref (io);
-
-  g_timeout_add_seconds (5, timeout_cb, app.loop);
 
   /* Iterate */
   g_print ("Running...\n");
-  g_main_loop_run (app.loop);
+  g_main_loop_run (app->loop);
 
   /* Out of the main loop, clean up nicely */
   g_print ("Returned, stopping playback\n");
-  gst_element_set_state (app.pipeline, GST_STATE_NULL);
+  gst_element_set_state (app->pipeline, GST_STATE_NULL);
 
   g_print ("Deleting pipeline\n");
-  gst_object_unref (GST_OBJECT (app.pipeline));
+  gst_object_unref (GST_OBJECT (app->pipeline));
   g_source_remove (bus_watch_id);
-  g_main_loop_unref (app.loop);
+  g_main_loop_unref (app->loop);
   unlink(control_pipe);
 
   return 0;
 }
-
-/*
-
-/bin/sh ~/gst/master/gstreamer/libtool --mode=link gcc -Wall gerald.c -o gerald $(pkg-config --cflags --libs gstreamer-1.0)
-
-*/
